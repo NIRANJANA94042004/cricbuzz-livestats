d1d2a220d3897451cc166d078dbd43171df0a665
NIRANJANA94042004 <nn4394@srmist.edu.in>
2026-04-22 12:23:26 +0530

    Update cricbuzz_api.py

--- api/cricbuzz_api.py
+++ api/cricbuzz_api.py
@@ -7,7 +7,7 @@ def get_live_matches():
     url = "https://cricbuzz-cricket.p.rapidapi.com/matches/v1/live"
 
     headers = {
-        "X-RapidAPI-Key": API_KEY,
+        "X-RapidAPI-Key":"ab9f8c4c75msh3e6c95d5768fba5p1c4681jsn753d5ae37a43" ,
         "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com"
     }
 
