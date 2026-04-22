9a371aa843086a9d3cf246d4d4bb5099bdc14be7
NIRANJANA94042004 <nn4394@srmist.edu.in>
2026-04-22 11:46:09 +0530

    Update cricbuzz_api.py

--- cricbuzz_api.py
+++ cricbuzz_api.py
@@ -1,12 +1,18 @@
 import requests
 
+API_KEY = "ab9f8c4c75msh3e6c95d5768fba5p1c4681jsn753d5ae37a43"
+
 def get_live_matches():
-    url = "https://cric-buzz-cricket-live.p.rapidapi.com/matches/live"
+    url = "https://cricbuzz-cricket.p.rapidapi.com/matches/v1/live"
 
     headers = {
-        "X-RapidAPI-Key": "ab9f8c4c75msh3e6c95d5768fba5p1c4681jsn753d5ae37a43",
-        "X-RapidAPI-Host": "cric-buzz-cricket-live.p.rapidapi.com"
+        "X-RapidAPI-Key":"ab9f8c4c75msh3e6c95d5768fba5p1c4681jsn753d5ae37a43",
+        "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com"
     }
 
     response = requests.get(url, headers=headers)
-    return response.json()
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"matches": []}
